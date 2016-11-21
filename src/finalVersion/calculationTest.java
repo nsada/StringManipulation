1997533bab1bd84c543d7f316f1e80e911de1f4b
Nsada <hitnstmy@163.com>
2016-11-21 21:16:16 +0800

    Test samples of white-box test

--- src/finalVersion/calculationTest.java
+++ src/finalVersion/calculationTest.java
@@ -8,14 +8,22 @@ public class calculationTest {
 
 	@Test
 	public void testJudgeFun() {		
-		assertEquals(true,calculation.judgeFun("x+yy^2*7+x*x"));
-		assertEquals(false,calculation.judgeFun("x+yy^x^2*7+x*x"));
+		assertEquals(false,calculation.judgeFun("35^3")); //1
+		assertEquals(true,calculation.judgeFun("35")); //2
+		assertEquals(false,calculation.judgeFun("x^3^2")); //3
+		assertEquals(true,calculation.judgeFun("x")); //4
+		assertEquals(false,calculation.judgeFun("x^yyy")); //5
+		assertEquals(true,calculation.judgeFun("x+yy^2*7+x*x")); //6
+		assertEquals(true,calculation.judgeFun("134*x")); //7
+		assertEquals(false,calculation.judgeFun("x(y")); //8
+		assertEquals(false,calculation.judgeFun("")); //9
+		assertEquals(false,calculation.judgeFun("*x+y")); //10
+		
 		assertEquals(false,calculation.judgeFun("x+yy^2^3*7+x*x"));
-		assertEquals(false,calculation.judgeFun(""));
+		assertEquals(false,calculation.judgeFun("x+yy^x^2*7+x*x"));		
+		assertEquals(true,calculation.judgeFun("x+yy^2*7+x*x"));
 		assertEquals(false,calculation.judgeFun("x+yy^2*7+x*x#"));
-		assertEquals(false,calculation.judgeFun("x+yy^2*7+)x*x"));
-		assertEquals(true,calculation.judgeFun("x+yyy"));
-		assertEquals(true,calculation.judgeFun("x+yy^2*7"));
+		assertEquals(false,calculation.judgeFun("x+yy^2*7+)x*x"));	
 	}
 
 }
