d6dd52a4051a691d946ba736680b19a855bcfb3d
Nsada <hitnstmy@163.com>
2016-11-28 12:48:57 +0800

    Lab7 Version0

---
+++ src/Lab7/Answer.java
@@ -0,0 +1,23 @@
+package Lab7;
+
+
+
+public class Answer {
+	private String answer;
+	
+	public Answer() {
+		answer = "";
+	}
+	
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+	
+	public String getAnswer() {
+		return this.answer;
+	}
+	
+	public void mergeAnswer(String answer) {
+		
+	}
+}
